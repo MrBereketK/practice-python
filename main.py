8007ae664a66dcdf29971aafa346ef613f40c615
Bereket Kiros <bereketkiros17@gmail.com>
2026-06-06 11:47:49 +0300

    Practice finding max and min scores in student records by Implementing score analysis using loops and built-in functions

--- main.py
+++ main.py
@@ -10,12 +10,34 @@ students = [
 for student in students:
     print(f"Name: {student['name']}, score: {student['score']}")
 
-for student in students:
-    total_score += student['score'];
+# for student in students:
+#     total_score += student['score'];
 
-average_score = total_score / len(students);
+# average_score = total_score / len(students);
 
-print(f"Total score: {total_score}")
-print(f"Average score: {average_score}")
+# print(f"Total score: {total_score}")
+# print(f"Average score: {average_score}")
+
+# using my own algorith to find highest and lowest score
+high = students[0]['score']
+for studnet in students:
+    if student['score'] > high:
+        high = student['score']
+
+        print(f"Highest score: {high}")
+
+low = students[0]['score']
+for studnet in students:
+    if student['score'] < low:
+        low = student['score']
+
+        print(f"Lowest score: {low}")
 
 
+# using built-in functions to find highest and lowest score
+highest_score = max(student['score'] for student in students)
+print(f"Highest score: {highest_score}")
+
+
+lowest_score = min(student['score'] for student in students)
+print(f"Lowest score: {lowest_score}")
